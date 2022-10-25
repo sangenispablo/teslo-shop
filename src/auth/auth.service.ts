08a90bd21d566e7c956b3e224f1c51f94a9daaba
Pablo Sangenis <sangenispablo@gmail.com>
2022-10-25 15:07:26 -0300

    Primera parte de Auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class AuthService {
+  create(createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+}
